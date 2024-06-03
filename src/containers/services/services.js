a68833731c4a26a6e0cd9887824e729cb0be0ac3
chetankb <chetankb619@gmail.com>
2024-06-03 18:31:12 +0800

    Updated services for UrbanFix

--- src/containers/services/services.js
+++ src/containers/services/services.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import HandymanIcon from '@mui/icons-material/Handyman';
 import ImagesearchRollerIcon from '@mui/icons-material/ImagesearchRoller';
-import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 import TvIcon from '@mui/icons-material/Tv';
-import LaptopMacIcon from '@mui/icons-material/LaptopMac';
 import HeatPumpIcon from '@mui/icons-material/HeatPump';
 import KitchenIcon from '@mui/icons-material/Kitchen';
-import MicrowaveIcon from '@mui/icons-material/Microwave';
-import LocalLaundryServiceIcon from '@mui/icons-material/LocalLaundryService';
 import FormatPaintIcon from '@mui/icons-material/FormatPaint';
-import HighlightIcon from '@mui/icons-material/Highlight';
-import CameraOutdoorIcon from '@mui/icons-material/CameraOutdoor';
-import AddHomeWorkIcon from '@mui/icons-material/AddHomeWork';
 import FormatColorFillIcon from '@mui/icons-material/FormatColorFill';
-import PaletteIcon from '@mui/icons-material/Palette';
-import FormatColorResetIcon from '@mui/icons-material/FormatColorReset';
 import ChaletIcon from '@mui/icons-material/Chalet';
-import LivingIcon from '@mui/icons-material/Living';
-import LocalConvenienceStoreIcon from '@mui/icons-material/LocalConvenienceStore';
 import FoundationIcon from '@mui/icons-material/Foundation';
-import AgricultureIcon from '@mui/icons-material/Agriculture';
-import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
-import HouseSidingIcon from '@mui/icons-material/HouseSiding';
+import LocalTaxiIcon from '@mui/icons-material/LocalTaxi';
+import CarRentalIcon from '@mui/icons-material/CarRental';
+import DepartureBoardIcon from '@mui/icons-material/DepartureBoard';
+import AirportShuttleIcon from '@mui/icons-material/AirportShuttle';
+import CommuteIcon from '@mui/icons-material/Commute';
+import EngineeringIcon from '@mui/icons-material/Engineering';
+import ElectricalServicesIcon from '@mui/icons-material/ElectricalServices';
+import CableIcon from '@mui/icons-material/Cable';
+import SettingsInputCompositeIcon from '@mui/icons-material/SettingsInputComposite';
+import PowerIcon from '@mui/icons-material/Power';
+import Diversity3Icon from '@mui/icons-material/Diversity3';
+import BrushIcon from '@mui/icons-material/Brush';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import WindPowerIcon from '@mui/icons-material/WindPower';
+import BlenderIcon from '@mui/icons-material/Blender';
 
 import { appliancesData } from '../../mock/appliances';
 
@@ -39,29 +41,29 @@ const Services = () => {
 	const icons = {
 		painting: <ImagesearchRollerIcon style={{ fontSize: '6em' }} />,
 		appliances: <HandymanIcon style={{ fontSize: '6em' }} />,
-		key_project: <ManageAccountsIcon style={{ fontSize: '6em' }} />,
+		key_project: <EngineeringIcon style={{ fontSize: '6em' }} />,
 		tv: <TvIcon style={{ fontSize: '6em' }} />,
 		ac: <HeatPumpIcon style={{ fontSize: '6em' }} />,
 		fridge: <KitchenIcon style={{ fontSize: '6em' }} />,
-		microwave_oven: <MicrowaveIcon style={{ fontSize: '6em' }} />,
-		washing_machine: <LocalLaundryServiceIcon style={{ fontSize: '6em' }} />,
-		laptop: <LaptopMacIcon style={{ fontSize: '6em' }} />,
-		wall_painting: <FormatPaintIcon style={{ fontSize: '6em' }} />,
-		wood_painting: <HighlightIcon style={{ fontSize: '6em' }} />,
-		cctv_services: <CameraOutdoorIcon style={{ fontSize: '6em' }} />,
-		interear_designers: <AddHomeWorkIcon style={{ fontSize: '6em' }} />,
+		man_power: <Diversity3Icon style={{ fontSize: '6em' }} />,
+		contract: <ApartmentIcon style={{ fontSize: '6em' }} />,
+		cab: <LocalTaxiIcon style={{ fontSize: '6em' }} />,
+		complete_painting: <FormatPaintIcon style={{ fontSize: '6em' }} />,
+		color_patch_work: <BrushIcon style={{ fontSize: '6em' }} />,
+		cctv_services: <ElectricalServicesIcon style={{ fontSize: '6em' }} />,
+		fan: <WindPowerIcon style={{ fontSize: '6em' }} />,
 		full_home_painting: <FormatColorFillIcon style={{ fontSize: '6em' }} />,
-		few_walls_painting: <PaletteIcon style={{ fontSize: '6em' }} />,
-		water_proofing: <FormatColorResetIcon style={{ fontSize: '6em' }} />,
+		power: <PowerIcon style={{ fontSize: '6em' }} />,
+		wiring: <SettingsInputCompositeIcon style={{ fontSize: '6em' }} />,
 		interior_and_exterior: <ChaletIcon style={{ fontSize: '6em' }} />,
-		interior_decorators: <LivingIcon style={{ fontSize: '6em' }} />,
-		site_preparation_phase: (
-			<LocalConvenienceStoreIcon style={{ fontSize: '6em' }} />
-		),
-		foundation_phase: <FoundationIcon style={{ fontSize: '6em' }} />,
-		framing_phase: <AgricultureIcon style={{ fontSize: '6em' }} />,
-		exterior_phase: <HouseSidingIcon style={{ fontSize: '6em' }} />,
-		interior_phase: <MeetingRoomIcon style={{ fontSize: '6em' }} />
+		patch_work: <CableIcon style={{ fontSize: '6em' }} />,
+		pickup_drop: <CommuteIcon style={{ fontSize: '6em' }} />,
+		planning: <FoundationIcon style={{ fontSize: '6em' }} />,
+		outstation: <AirportShuttleIcon style={{ fontSize: '6em' }} />,
+		tour_package: <DepartureBoardIcon style={{ fontSize: '6em' }} />,
+		car_rental: <CarRentalIcon style={{ fontSize: '6em' }} />,
+		supervision: <ManageAccountsIcon style={{ fontSize: '6em' }} />,
+		mixture: <BlenderIcon style={{ fontSize: '6em' }} />
 	};
 
 	const handleOpen = (key) => {
