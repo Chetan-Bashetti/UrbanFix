4ebc771be6a86bbdf575ab37991edd8b800e2290
chetankb <chetankb619@gmail.com>
2024-06-09 01:02:08 +0800

    Updated Review given by cleint

--- src/mock/reviews.js
+++ src/mock/reviews.js
@@ -1,36 +1,44 @@
 export const reviews = [
 	{
-		user: 'Nanditha SR',
+		user: 'Dr. Sumit',
+		gender: 'm',
+		rating: 4,
+		review:
+			'Delivers top-notch urban services with professionalism and efficiency. Highly recommended for all your urban needs!'
+	},
+	{
+		user: 'Pavan Hondi',
+		gender: 'm',
+		rating: 5,
+		review:
+			'UrbanFix paints exceeded my expectations! Their range of colors is vibrant and long-lasting, transforming my space into a work of art. The application process was smooth, and the finish was flawless. I highly recommend UrbanFix paints for anyone looking to elevate their space with quality and style.'
+	},
+	{
+		user: 'Chetan kumar',
+		gender: 'm',
+		rating: 4,
+		review:
+			"UrbanFix's teamwork is unmatched! Their seamless collaboration and expertise ensured a flawless outcome. Highly recommended for any urban project!"
+	},
+	{
+		user: 'Rihana',
 		gender: 'f',
 		rating: 4,
 		review:
-			'Fantabulous timely service! mainly focused on customer demand as well as their satisfaction.'
+			'UrbanFix ki service lajawab hai, kaam ko tezi se aur professionalism ke saath poora kiya gaya. Sab urban zarooraton ke liye highly recommend kiya jata hai!'
 	},
 	{
-		user: 'enzin choezin',
-		gender: 'm',
-		rating: 5,
-		review: 'Awesome service! utmost good faith amongst the members. '
-	},
-	{
-		user: 'chinmay deborra',
-		gender: 'm',
-		rating: 4,
-		review:
-			'Very good timely responded service and also all the employees and service centre has a friendly relationship with utmost customer satisfaction.'
-	},
-	{
-		user: 'raghavendra chakraborthy',
-		gender: 'm',
-		rating: 4,
-		review:
-			'Outstanding service provider with the motive of attaining client contentment.'
-	},
-	{
-		user: 'Nadiya nigam',
+		user: 'Danish',
 		gender: 'm',
 		rating: 5,
 		review:
-			'Happy service provider, with attentive and knowledgeable staffs providing excellent recommendations and ensuring our utmost satisfaction. recommended for all.'
+			'UrbanFix ka kaam ekdum mast hai! Unki team ne meri samasya ko turant suljha diya aur kaam bhi badiya tarike se kiya. Main unki service ko dil se recommend karta hoon, agar aapko urban samasyaon ka solution chahiye toh.'
+	},
+	{
+		user: 'Dr mehtab',
+		gender: 'm',
+		rating: 5,
+		review:
+			"UrbanFix's teamwork is exceptional, making the entire process smooth and hassle-free. Highly recommended for their dedication and efficiency!"
 	}
 ];
